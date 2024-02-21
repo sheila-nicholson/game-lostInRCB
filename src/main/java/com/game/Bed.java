b3614c451cc4ede134931b62fd03c6375da42f00
sheila <sheila_nicholson@sfu.ca>
2024-02-21 10:40:20 -0800

    created bed class and added basic variables/methods

--- src/main/java/com/game/Bed.java
+++ src/main/java/com/game/Bed.java
@@ -1,4 +1,20 @@
 package com.game;
 
-public class Bed {
+import java.awt.*;
+
+public class Bed extends rewardItem {
+
+    private String rewardType = "Bed";
+    private int scoreBonus = 5;
+
+    /**
+     * Constructor
+     */
+    public Bed() {
+    }
+
+    public int getScoreModifier(){
+        return this.scoreBonus;
+    }
+
 }
