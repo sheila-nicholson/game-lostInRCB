123ae0ec37e5b61ee5a82a46e7d2fdfab20ea533
sheila <sheila_nicholson@sfu.ca>
2024-02-21 10:50:15 -0800

    Created APlusPaper class and added basic variables/methods

---
+++ src/main/java/com/game/APlusPaper.java
@@ -0,0 +1,22 @@
+package com.game;
+
+public class APlusPaper extends RewardItem {
+
+    private String rewardType = "A+ Paper";
+    private int scoreBonus = 10;
+    private int timeAvaliable;
+
+    /**
+     * Constructor
+     */
+    public APlusPaper() {
+    }
+
+    public int getScoreModifier(){
+        return this.scoreBonus;
+    }
+
+    public int getTimeAvaliable() {
+        return this.timeAvaliable;
+    }
+}
