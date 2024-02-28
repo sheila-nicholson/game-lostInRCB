7ff3bc3439db23591c033472fce85ca51db9054b
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:53:19 -0800

    Update CharacterFactory.java
    documentation

--- src/main/java/com/game/Factory/CharacterFactory.java
+++ src/main/java/com/game/Factory/CharacterFactory.java
@@ -1,3 +1,13 @@
+/*
+ * CharacterFactory.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Factory;
 
 import com.game.Character.Enemy;
