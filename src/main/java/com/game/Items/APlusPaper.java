e3c78ac4ecdd80173318cb24efc15c0a0d233edf
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:56:11 -0800

    Update APlusPaper.java
    documentation

--- src/main/java/com/game/Items/APlusPaper.java
+++ src/main/java/com/game/Items/APlusPaper.java
@@ -1,3 +1,13 @@
+/*
+ * APlusPaper.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Items;
 
 public class APlusPaper extends RewardItem {
