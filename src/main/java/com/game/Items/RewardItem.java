c946719ce23be46c4d222eaa41c215d0b504721d
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:58:53 -0800

    Update RewardItem.java
    documentation

--- src/main/java/com/game/Items/RewardItem.java
+++ src/main/java/com/game/Items/RewardItem.java
@@ -1,3 +1,13 @@
+/*
+ * RewardItem.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Items;
 
 public abstract class RewardItem extends Item {
