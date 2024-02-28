67121fa3ec1f5466e298ee1649189318329589ce
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:55:02 -0800

    Update EasyCharacterFactory.java
    documentation + comments

--- src/main/java/com/game/Factory/EasyCharacterFactory.java
+++ src/main/java/com/game/Factory/EasyCharacterFactory.java
@@ -1,10 +1,20 @@
+/*
+ * EasyCharacterFactory.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Factory;
 
 import com.game.Character.*;
 
 public class EasyCharacterFactory extends CharacterFactory {
     public Hero createHero() {
-        return Hero.getInstance(150); //temp speed
+        return Hero.getInstance(90); //temp speed - fastest speed throughout all levels? subject to change?
     }
     public Enemy createEnemy() {
             return Raccoon.getInstance(100);
