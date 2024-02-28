f2470ddd580c0915d06e022a760664be216c0db1
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:59:29 -0800

    Update Vortex.java
    documentation + comments

--- src/main/java/com/game/Items/Vortex.java
+++ src/main/java/com/game/Items/Vortex.java
@@ -1,3 +1,13 @@
+/*
+ * Vortex.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+3
 package com.game.Items;
 
 import com.game.Position;
@@ -5,7 +15,7 @@ import com.game.Position;
 public class Vortex extends PunishmentItem {
 
     private String punishmentType = "Vortex";
-    private int damagePoints = 5;
+    private int damagePoints = 5; // or just make it a signed int that can go negative to keep it to one system of points perhaps? (copy pasted from pileofbooks)
 
     /**
      * Constructor
