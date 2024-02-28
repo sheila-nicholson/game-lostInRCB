431cdffe67752998587da6b950e957828cadf959
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:55:40 -0800

    Update MediumCharacterFactory.java
    documentation + comments

--- src/main/java/com/game/Factory/MediumCharacterFactory.java
+++ src/main/java/com/game/Factory/MediumCharacterFactory.java
@@ -1,3 +1,13 @@
+/*
+ * MediumCharacterFactory.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Factory;
 
 import com.game.Character.Enemy;
@@ -6,7 +16,7 @@ import com.game.Character.ZoombieProfessor;
 
 public class MediumCharacterFactory extends CharacterFactory {
     public Hero createHero() {
-        return Hero.getInstance(200); //temp speed
+        return Hero.getInstance(80); //temp speed - see easy + difficult for why it's 80
     }
     public Enemy createEnemy() {
         return ZoombieProfessor.getInstance(100); //temp speed
