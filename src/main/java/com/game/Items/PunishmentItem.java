6b548bff7f5f5cdccb2a32084c97bdc2a0538e14
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:58:39 -0800

    Update PunishmentItem.java
    documentation

--- src/main/java/com/game/Items/PunishmentItem.java
+++ src/main/java/com/game/Items/PunishmentItem.java
@@ -1,3 +1,13 @@
+/*
+ * PunishmentItem.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Items;
 
 public abstract class PunishmentItem extends Item {
