23f81a9dc049e8784196190ee436fcc2812ac146
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 05:02:52 -0800

    Update Tile.java
    documentation

--- src/main/java/com/game/Tile/Tile.java
+++ src/main/java/com/game/Tile/Tile.java
@@ -1,3 +1,13 @@
+/*
+ * Tile.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+3
+
 package com.game.Tile;
 
 import com.game.Position;
