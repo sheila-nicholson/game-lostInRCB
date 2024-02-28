7d5a19fc617583c785b5f63f4f10f260fa4c180b
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:49:37 -0800

    Update Character.java
    adding documentation + comments

--- src/main/java/com/game/Character/Character.java
+++ src/main/java/com/game/Character/Character.java
@@ -1,3 +1,12 @@
+/*
+ * Character.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
 package com.game.Character;
 
 import com.game.Position;
@@ -19,10 +28,12 @@ public abstract class Character extends Position {
     }
 
     public void moveCharacter() {
+        // move along array per game tick
         return;
     }
 
     public void displayCharacter() {
+        // should be visible at all times XOR in vortex teleport animation (quick milliseconds?)
         return;
     }
 
