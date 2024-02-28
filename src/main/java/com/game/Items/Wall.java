ecee18189056fd2ecce296496071efda7b51e563
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 05:00:09 -0800

    Update Wall.java
    documentation

--- src/main/java/com/game/Items/Wall.java
+++ src/main/java/com/game/Items/Wall.java
@@ -1,3 +1,13 @@
+/*
+ * Wall.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Items;
 
 import com.game.Position;
