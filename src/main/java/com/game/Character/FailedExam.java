1d55c5e68f6d3a6e2f5ab0d129787b6b21ac6184
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:50:25 -0800

    Update FailedExam.java
    documentation

--- src/main/java/com/game/Character/FailedExam.java
+++ src/main/java/com/game/Character/FailedExam.java
@@ -1,3 +1,13 @@
+/*
+ * FailedExam.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Character;
 
 public class FailedExam extends Enemy {
