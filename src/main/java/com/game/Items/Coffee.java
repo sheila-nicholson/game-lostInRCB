ae67e46c7d61a82f91ca7d6d544897d373661329
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:57:04 -0800

    Update Coffee.java
    documentation + comments

--- src/main/java/com/game/Items/Coffee.java
+++ src/main/java/com/game/Items/Coffee.java
@@ -1,9 +1,19 @@
+/*
+ * Coffee.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Items;
 
 public class Coffee extends RewardItem {
 
     private String rewardType = "Coffee";
-    private int scoreBonus = 5;
+    private int scoreBonus = 5; // unsure, temporary value
     private int modifierSeconds = 5;
 
     /**
