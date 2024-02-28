bbb5540cb87cb8aa670f4a31e1077017a78c1dc4
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:51:58 -0800

    Update Raccoon.java
    documentation + comments

--- src/main/java/com/game/Character/Raccoon.java
+++ src/main/java/com/game/Character/Raccoon.java
@@ -1,6 +1,18 @@
+/*
+ * Raccoon.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Character;
 public class Raccoon extends Enemy {
     protected Raccoon(int speed){
         super(speed);
     }
 }
+
+// implement possible balancing numbers discussed in phase 1?
