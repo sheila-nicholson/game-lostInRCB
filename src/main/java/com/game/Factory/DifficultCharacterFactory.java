be4a898bd0332d7acbf296315d9381d30c254861
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:54:12 -0800

    Update DifficultCharacterFactory.java
    documentation + comments

--- src/main/java/com/game/Factory/DifficultCharacterFactory.java
+++ src/main/java/com/game/Factory/DifficultCharacterFactory.java
@@ -1,9 +1,20 @@
+/*
+ * DifficultCharacterFactory.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Factory;
 import com.game.Character.*;
 
 public class DifficultCharacterFactory extends CharacterFactory {
     public Hero createHero() {
         return Hero.getInstance(100); //temp speed
+        // hero should be slower than enemies throughout ALL stages of difficulty
     }
 
     public Enemy createEnemy() {
@@ -14,3 +25,4 @@ public class DifficultCharacterFactory extends CharacterFactory {
 
     }
 }
+
