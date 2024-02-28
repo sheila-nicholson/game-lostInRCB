85b4814a3828d0c7bd33fee3b0287a43d8872552
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:58:22 -0800

    Update PileOfBooks.java
    documentation + comments

--- src/main/java/com/game/Items/PileOfBooks.java
+++ src/main/java/com/game/Items/PileOfBooks.java
@@ -1,9 +1,19 @@
+/*
+ * PileOfBooks.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
 package com.game.Items;
 
 public class PileOfBooks extends RewardItem {
 
     private String punishmentType = "Pile of books";
-    private int damagePoints = 5;
+    private int damagePoints = 5; // or just make it a signed int that can go negative to keep it to one system of points perhaps?
 
     /**
      * Constructor
