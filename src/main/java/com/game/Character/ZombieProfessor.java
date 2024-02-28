853202e7c36ad6bef0ce9770d76943c0477b825e
Jonas Lam <jonas_lam@sfu.ca>
2024-02-28 04:52:53 -0800

    modify
    documentation + comments + typo fix

---
+++ src/main/java/com/game/Character/ZombieProfessor.java
@@ -0,0 +1,18 @@
+/*
+ * ZombieProfessor.java
+ * 
+ * Class Description: [to come Wednesday evening by Jonas]
+ *
+ * Authors: [put your names here]
+ * Last modified on: February 28 4:48 AM
+ */
+
+
+package com.game.Character;
+public class ZombieProfessor extends Enemy {
+    protected ZombieProfessor (int speed){
+        super(speed);
+    }
+}
+
+// presumable typo? Zoombie -> Zombie
--- src/main/java/com/game/Character/ZoombieProfessor.java
+++
@@ -1,6 +0,0 @@
-package com.game.Character;
-public class ZoombieProfessor extends Enemy {
-    protected ZoombieProfessor (int speed){
-        super(speed);
-    }
-}
