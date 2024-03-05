28fe33e6d89b8a82c49bd4f2d32e45a105efe0bb
Jonas Lam <jonas_lam@sfu.ca>
2024-03-05 11:06:53 -0800

    Update Enemy.java
    Update doc + added stuff from UML + new methods

--- src/main/java/com/game/Character/Enemy.java
+++ src/main/java/com/game/Character/Enemy.java
@@ -1,7 +1,8 @@
 /*
  * Enemy.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Main attributes of all three enemies who will chase the player.
+ *                    If they make contact with the character the game ends in a loss.
  *
  * Authors: [put your names here]
  * Last modified on: February 28 4:48 AM
@@ -14,9 +15,17 @@ import java.awt.*;
 import java.io.IOException;
 
 public class Enemy extends Character {
+
+    protected int damagePoints = 0;
+    protected int movementSpeed = 0; // variable based on level
+    
     protected static Enemy instance = null;
     protected void setDefaultPosition(){
-        this.setPosition(0,100);//temp
+        this.setPosition(0,100); // temporary value at NW corner
+        // easier to set enemy's position consistently since there's three different
+        // enemies (one for each level)
+        // upon selecting difficulty level either character's position is also
+        // static or spawns closer to the enemy with each harder difficulty
     }
 
     protected Enemy(int speed){
@@ -32,10 +41,36 @@ public class Enemy extends Character {
     public void update(){};
     public void draw (Graphics2D g2){};
     public void getImage(){
+
 //        try{
 //
 //        }catch(IOException e){
 //            e.printStackTrace();
 //        }
     };
+
+    // call moveCharacter with traversing salesman algorithm to hunt for Hero
+
+    public void damagePlayer() {
+/* 
+        if (position of enemy == position of hero) {
+            end the current level with current score and display result
+        }
+
+    */
+
+    }
+
+    public int scoreModifier() {
+
+/*
+ *      if (game is ended) {
+ * 
+ *          damagePoints -= 20; // arbitrary value?
+ * 
+ *      }
+ * 
+ */
+
+    }
 }
