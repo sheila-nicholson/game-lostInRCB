d4284f5ceb56d0617a01c484f2757b9fa9cb44ac
Jonas Lam <jonas_lam@sfu.ca>
2024-03-05 12:01:20 -0800

    Update ZombieProfessor.java
    added to keep consistency with all other enemies (raccoon + exam)

--- src/main/java/com/game/Character/ZombieProfessor.java
+++ src/main/java/com/game/Character/ZombieProfessor.java
@@ -1,10 +1,11 @@
 /*
  * ZombieProfessor.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Describing the medium boss on the medium level.
+ *                    Medium speed and medium score penalty.
  *
  * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Last modified on: March 5, 12:00 PM
  */
 
 
@@ -12,5 +13,9 @@ package com.game.Character;
 public class ZombieProfessor extends Enemy {
     protected ZombieProfessor (int speed){
         super(speed);
+        // set speed to lowest amount of all three bosses
+        // speed and damagePoints inherited from Enemy and will vary
     }
+
+    // call scoreModifier
 }
