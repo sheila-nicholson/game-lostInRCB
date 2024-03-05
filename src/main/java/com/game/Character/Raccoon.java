e41eb4025050ed96fda13658ad13f9f190f89af5
Jonas Lam <jonas_lam@sfu.ca>
2024-03-05 11:59:28 -0800

    Update Raccoon.java
    removed unnecessary

--- src/main/java/com/game/Character/Raccoon.java
+++ src/main/java/com/game/Character/Raccoon.java
@@ -1,10 +1,11 @@
 /*
  * Raccoon.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Describing the easiest boss on the easiest level.
+ *                    Slowest speed and lowest score penalty.
  *
  * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Last modified on: March 5, 12:00 PM
  */
 
 
@@ -12,7 +13,9 @@ package com.game.Character;
 public class Raccoon extends Enemy {
     protected Raccoon(int speed){
         super(speed);
+        // set speed to lowest amount of all three bosses
+        // speed and damagePoints inherited from Enemy and will vary
     }
-}
 
-// implement possible balancing numbers discussed in phase 1?
+        // call scoreModifier
+}
