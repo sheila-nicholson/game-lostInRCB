f4fb0149e938b678a5fdfdfa322e8aff2cbcbd47
Jonas Lam <jonas_lam@sfu.ca>
2024-03-05 11:28:05 -0800

    Update FailedExam.java
    Update doc + add speed

--- src/main/java/com/game/Character/FailedExam.java
+++ src/main/java/com/game/Character/FailedExam.java
@@ -1,18 +1,23 @@
 /*
  * FailedExam.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Describing the hardest boss on the hardest level.
+ *                    Fastest speed and biggest score penalty.
  *
  * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Last modified on: March 5 11:27 AM
  */
 
 
 package com.game.Character;
 
 public class FailedExam extends Enemy {
+
     protected FailedExam(int speed){
-        super(speed);
+        super(speed); // set speed to lowest amount of all three bosses
+        // speed and damagePoints inherited from Enemy and will vary
     }
+
+    // call scoreModifier
 
 }
