b92e3064da59787af3a831c1b85d522177b7fed5
Jonas Lam <jonas_lam@sfu.ca>
2024-03-06 01:35:14 -0800

    Update BadTile.java
    implementation + added based on UML

--- src/main/java/com/game/Tile/BadTile.java
+++ src/main/java/com/game/Tile/BadTile.java
@@ -1,10 +1,10 @@
 /*
  * BadTile.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Details all tiles that if stepped on provide a negative effect to the player (such as punishment items)
  *
- * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Authors: [put your names here] + Brendan
+ * Last modified on: March 6, 2024 1:30 AM
  */
 
 
@@ -16,6 +16,8 @@ import java.awt.*;
 
 public class BadTile extends Tile {
     private PunishmentItem tileItem;
+    private Image tileSprite;
+    private boolean tileSteppability;
 
     BadTile(PunishmentItem param_item, Image param_image, int param_X, int param_y){
         this.tileItem = param_item;
@@ -23,7 +25,15 @@ public class BadTile extends Tile {
         setTileSprite(param_image);
     }
 
+    public Item getTileItem() {
+        return this.tileItem;
+    }
+
     public PunishmentItem getTileItem(){
         return this.tileItem;
+    }
+
+    public boolean tileSteppable() {
+        return this.tileSteppability;
     }
 }
