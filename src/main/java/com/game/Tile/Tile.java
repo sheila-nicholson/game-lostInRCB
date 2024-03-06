7f6b296f05e733465c6d1d426f149812a60be3d8
Jonas Lam <jonas_lam@sfu.ca>
2024-03-06 01:49:43 -0800

    Update Tile.java
    updated methods + consistency

--- src/main/java/com/game/Tile/Tile.java
+++ src/main/java/com/game/Tile/Tile.java
@@ -1,10 +1,10 @@
 /*
  * Tile.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Sums up all main attributes of all tiles (all types) in the game.
  *
- * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Authors: [put your names here] + Brendan
+ * Last modified on: March 6, 2024 1:48 AM
  */
 
 
@@ -16,11 +16,16 @@ import java.awt.*;
 
 public abstract class Tile extends Position {
     private Image tileSprite;
+    private boolean tileSteppable;
 
     public Image getTileSprite() {
         return this.tileSprite;
     }
     public void setTileSprite(Image param_sprite){
         this.tileSprite = param_sprite;
+    }
+
+    public boolean tileSteppable() {
+        return this.tileSteppable;
     }
 }
