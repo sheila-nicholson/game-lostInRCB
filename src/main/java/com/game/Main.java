375014638b218838a08798a2c6e667e1ab4ef4d1
Jonas Lam <jonas_lam@sfu.ca>
2024-03-06 01:53:45 -0800

    Revert "Update Main.java"
    This reverts commit 39fe9f90f5de83e9bc1c42fe54b661d3bd961ea0.

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -1,7 +1,7 @@
 /*
  * Game.java
  * 
- * Class Description: Main class, where all the stuff happens. 
+ * Class Description: [to come Wednesday evening by Jonas]
  *
  * Authors: [put your names here]
  * Last modified on: February 28 4:48 AM
