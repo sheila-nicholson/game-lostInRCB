34e9a1ccc05058f2e29b96ceee886ed510a2c6ea
Jonas Lam <jonas_lam@sfu.ca>
2024-03-06 01:18:15 -0800

    Update PunishmentItem.java
    update doc + variable method

--- src/main/java/com/game/Items/PunishmentItem.java
+++ src/main/java/com/game/Items/PunishmentItem.java
@@ -1,17 +1,17 @@
 /*
  * PunishmentItem.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Main subcategory of any items that have a negative effect on the player.
  *
- * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Authors: [put your names here] + Sheila ???
+ * Last modified on: March 6, 2024 1:16 AM
  */
 
 
 package com.game.Items;
 
 public abstract class PunishmentItem extends Item {
-    private int damagePoints;
+    private int damagePoints; // only applicable to pileOfBooks - vortex and smoke don't penalize points
 
     public int getScoreModifier(){
         return this.damagePoints;
