de29f7ad8ed84f8bad692349409019cacec8654f
Jonas Lam <jonas_lam@sfu.ca>
2024-03-06 01:27:13 -0800

    Update Wall.java
    added variables and fixed typoes

--- src/main/java/com/game/Items/Wall.java
+++ src/main/java/com/game/Items/Wall.java
@@ -1,19 +1,23 @@
 /*
  * Wall.java
  * 
- * Class Description: [to come Wednesday evening by Jonas]
+ * Class Description: Describes wall objects such as the boundaries of the map.
  *
- * Authors: [put your names here]
- * Last modified on: February 28 4:48 AM
+ * Authors: [put your names here] + Sheila ???
+ * Last modified on: March 6, 2024 1:25 AM
  */
-
 
 package com.game.Items;
 
 import com.game.Position;
 
 public class Wall extends Position {
+
+    private int wallLength = 1; // temporary value
+    private Image wallSprite;
+
     Wall(int param_X, int param_y){
+
         setPosition(param_X,param_y);
     }
 }
