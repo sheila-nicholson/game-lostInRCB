7b2abbd107c40c02d93016815f9d33f8855e7db4
Jonas Lam <jonas_lam@sfu.ca>
2024-03-11 19:53:39 -0700

    Update UI.java
    revert

--- src/main/java/com/game/UI.java
+++ src/main/java/com/game/UI.java
@@ -18,8 +18,6 @@ import java.awt.Graphics2D;
 
 public class UI {
 
-    private Hero hero;
-    
     GamePanel gp;
     Font a40;
     public boolean visibleText = false;
@@ -43,7 +41,7 @@ public class UI {
 
         gr2D.setFont(a40);
         gr2D.setColor(Color.BLUE);
-        gr2D.drawString("Score =" + hero.getScore(), 150,40);
+        gr2D.drawString("Score =",150,40);
 
         if (visibleText) {
             gr2D.setFont(gr2D.getFont().deriveFont(24F));
