a2b8215e463a370c569221d54547b6e801a1ab02
Jonas Lam <jonas_lam@sfu.ca>
2024-03-11 20:20:47 -0700

    Update UI.java
    added timer + formatting

--- src/main/java/com/game/UI.java
+++ src/main/java/com/game/UI.java
@@ -15,18 +15,22 @@ import com.game.GamePanel.GamePanel;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.text.DecimalFormat;
 
 public class UI {
 
     GamePanel gp;
-    Font a40;
+    Font a40, a60;
     public boolean visibleText = false;
     public String words = "";
     int wordsTime = 0;
+    double timer;
+    DecimalFormat deForm = new DecimalFormat("#0.00");
 
     public UI(GamePanel gp) {
         this.gp = gp;
-        a40 = new Font("Arial", Font.BOLD, 40);
+        a40 = new Font("Arial", Font.PLAIN, 40);
+        a60 = new Font("Arial", Font.BOLD, 60);
     }
 
     public void showMessage(String text) {
@@ -37,9 +41,30 @@ public class UI {
 
     public void draw(Graphics2D gr2D) {
 
+        /* UNCOMMENT THIS FOR GAME OVER
+        if (gameFinished) {
+            gr2D.setFont(a60);
+            gr2D.setColor(Color.GREEN);
+
+            String txt;
+            int txtLen;
+            int x, y;
+
+            txt = "Game Over" // add conditionals for victory or loss
+            txtLen = (int)gr2D.getFontMetrics().getStringBounds(txt, gr2D).getWidth();
+            x = gp.screenWidth/2 - txtLen/2;
+            y = gp.screenHeight/2 - (gp.tileSize*3);
+            gr2D.drawString(txt, x, y);
+
+            gp.gameThread = null;
+        }
+        */
         gr2D.setFont(a40);
         gr2D.setColor(Color.BLUE);
         gr2D.drawString("Score = WIP",150,40);
+
+        timer +=(double)1/60;
+        gr2D.drawString("Time:"+deForm.format(timer), gp.tileSize*11, 65);
 
         if (visibleText) {
             gr2D.setFont(gr2D.getFont().deriveFont(24F));
