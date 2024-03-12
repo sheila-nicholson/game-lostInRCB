e26a9b6c64f33e8b79df83a0b45ef86b07ad91c5
Jonas Lam <jonas_lam@sfu.ca>
2024-03-11 18:32:48 -0700

    update UI.java
    added some basic implementation

--- src/main/java/com/game/UI.java
+++ src/main/java/com/game/UI.java
@@ -9,6 +9,25 @@
 
 package com.game;
 
+import com.game.GamePanel.GamePanel;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
 public class UI {
-    // testing
+    GamePanel gp;
+    Font a40;
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+        a40 = new Font("Arial", Font.BOLD, 40);
+    }
+
+    public void draw(Graphics2D gr2D) {
+
+        gr2D.setFont(a40);
+        gr2D.setColor(Color.CYAN);
+        //gr2D.drawString("HI GUYS WELCOME TO MY MINECRAFT WORLD" + )
+    }
 }
