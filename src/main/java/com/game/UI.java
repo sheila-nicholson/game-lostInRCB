adbcfe2589e5ee4e1ce0ff22aa304578edd36cab
Jonas Lam <jonas_lam@sfu.ca>
2024-03-11 17:57:35 -0700

    Create UI.java
    testing

---
+++ src/main/java/com/game/UI.java
@@ -0,0 +1,14 @@
+/*
+ * UI.java
+ *
+ * Class Description: Handles the UI aspect
+ *
+ * Authors: Jonas
+ * Last modified on: March 11, 2014 5:57 PM
+ */
+
+package com.game;
+
+public class UI {
+    // testing 
+}
