f16cbb09e1bb8d97a2ea2a314806deb79eff4b53
yqa32 <yanjun_qian@sfu.ca>
2024-03-31 15:53:53 -0700

    add test case for hero movement

--- src/test/java/testHeroMovement.java
+++ src/test/java/testHeroMovement.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class testHeroMovement {
+import com.game.Character.Hero;
+import com.game.GamePanel.GamePanel;
+import com.game.Key.KeyHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testHeroMovement {
+
+    private Hero hero;
+
+    @BeforeEach
+    void setHero(){
+        GamePanel gamePanel = new GamePanel();
+        KeyHandler keyHandler = new KeyHandler(gamePanel);
+        hero = Hero.getInstance(4, keyHandler,gamePanel);
+    }
+
+    @Test
+    void testHeroMoveRight(){
+        int expectX = hero.getXPosition() + hero.getMovementSpeed();
+        hero.moveRight(hero.getMovementSpeed());
+        assertEquals(expectX,hero.getXPosition());
+    }
+
+    @Test
+    void testHeroMoveLeft(){
+        int expectX = hero.getXPosition() - hero.getMovementSpeed();
+        hero.moveLeft(hero.getMovementSpeed());
+        assertEquals(expectX,hero.getXPosition());
+
+    }
+
+    @Test
+    void testHeroMoveUp(){
+        int expectY = hero.getYPosition() - hero.getMovementSpeed();
+        hero.moveUp(hero.getMovementSpeed());
+        assertEquals(expectY,hero.getYPosition());
+
+    }
+
+    @Test
+    void testHeroMoveDown(){
+        int expectY = hero.getYPosition() + hero.getMovementSpeed();
+        hero.moveDown(hero.getMovementSpeed());
+        assertEquals(expectY,hero.getYPosition());
+    }
+
 }
