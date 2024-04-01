9d56b226457825e6b58c28105203d02826bcd5b9
yqa32 <yanjun_qian@sfu.ca>
2024-03-31 20:53:39 -0700

    add testing for hero movement

---
+++ src/main/java/com/game/Items/ItemType.java
@@ -0,0 +1,2 @@
+package com.game.Items;public enum ItemType {
+}
--- src/test/java/TestHeroMovement.java
+++ src/test/java/TestHeroMovement.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class testHeroMovement {
+public class TestHeroMovement {
 
     private Hero hero;
 
