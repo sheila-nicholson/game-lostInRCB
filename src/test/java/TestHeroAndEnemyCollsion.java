2c2f0652250385c57bf0f93d9ff86983d436ba25
yqa32 <yanjun_qian@sfu.ca>
2024-04-06 14:01:47 -0700

    test collision between hero and enemy in different position and direction

--- src/test/java/TestHeroAndEnemyCollsion.java
+++ src/test/java/TestHeroAndEnemyCollsion.java
@@ -2,8 +2,6 @@ import com.game.Characters.Enemy;
 import com.game.Characters.Hero;
 import com.game.CollisionChecker;
 import com.game.GamePanel.GamePanel;
-import com.game.Items.APlusPaper;
-import com.game.Items.*;
 import com.game.Key.Direction;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,11 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 
-public class CollisionCheckerTest {
+public class TestHeroAndEnemyCollsion {
 
     private CollisionChecker collisionChecker;
     private GamePanel gamePanel;
-    //private Item[] items;
     private String difficulty = "easy";
     private Hero hero;
     private Enemy enemy;
@@ -31,41 +28,6 @@ public class CollisionCheckerTest {
         enemy = gamePanel.getEnemy();
 
     }
-
-    @AfterEach
-    void reset(){
-        gamePanel.closeGamePanel();
-    }
-
-    @Test
-    public void itemPlacementAllowed(){
-
-        APlusPaper testItem = new APlusPaper(gamePanel);
-        testItem.setPosition(1, 18);
-        APlusPaper testItem2 = new APlusPaper(gamePanel);
-        testItem.setPosition(50, 50);
-        Item[] itemArr = {testItem2};
-        itemArr[0] = null;
-        boolean validPosition;
-        validPosition = !collisionChecker.isHeroIntersecting(testItem) &&
-                !collisionChecker.isTileOccupied(itemArr, testItem);
-        assertTrue(validPosition);
-    }
-
-    @Test
-    public void itemPlacementNotAllowed(){
-
-        APlusPaper testItem = new APlusPaper(gamePanel);
-        testItem.setPosition(1, 0);
-        APlusPaper testItem2 = new APlusPaper(gamePanel);
-        testItem2.setPosition(1, 0);
-        Item[] itemArr = {testItem2};
-        boolean validPosition;
-        validPosition = !collisionChecker.isHeroIntersecting(testItem) && !collisionChecker.isTileOccupied(itemArr, testItem);
-        assertFalse(validPosition);
-
-    }
-
 
     //tests for collisions between hero and enemy
     @Test
@@ -116,9 +78,5 @@ public class CollisionCheckerTest {
 
         assertTrue(collisionChecker.checkCharacter(enemy,hero) != 999);
 
-        enemy.checkCollision();
     }
-
-
-
 }
